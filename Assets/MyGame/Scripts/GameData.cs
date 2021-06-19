556e237a6d6102aabe70d23850197636efdf5f9d
LenaCupid <noah.schatzl01@htl-salzburg.ac.at>
2021-06-19 10:37:03 +0200

    ???

---
+++ Assets/MyGame/Scripts/GameData.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "GameData")]
+
+public class GameData : ScriptableObject, ISerializationCallbackReceiver
+{
+
+    public bool isDoorOpened = false;
+    public bool isPlayerInMaze = false;
+    public void OnAfterDeserialize()
+    {
+        isDoorOpened = false;
+        isPlayerInMaze = false;
+}
+
+    public void OnBeforeSerialize()
+    {
+        
+    }
+
+}
---
+++ Assets/MyGame/Scripts/MazeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeController : MonoBehaviour
+{
+
+    public GameData myGameData;
+    public GameObject entrance;
+
+    private Script_AnimateDoor entranceAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        entranceAnim = entrance.GetComponent<Script_AnimateDoor>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (myGameData.isDoorOpened && myGameData.isPlayerInMaze)
+        {
+
+        }
+
+    }
+}
